3bc9e9f7448a844afb63ff39ce89a0316b81c5f7
Samuel  Ellicott <sellicott@cedarville.edu>
2019-01-23 14:35:22 -0500

    Added Register startup values

--- motor-controller-nucleo/Core/Src/MotorControllerInterface.cpp
+++ motor-controller-nucleo/Core/Src/MotorControllerInterface.cpp
@@ -2,6 +2,7 @@
 #include <stm32f3xx_hal.h>
 // from USB_DEVICE/App directory
 #include <usbd_cdc_if.h>
+#include <TMC4671.h>
 
 // define commonly used types
 using int8_t   = std::int8_t;
@@ -46,6 +47,10 @@ void MotorControllerInterface::transmit_setting(MotorControllerParameter_t param
       packet.data.u8 = Settings->MotorDir;
     break;
 
+    case MotorParams::MOTOR_TYPE:
+      packet.rw_address = static_cast<uint8_t>(param);
+      packet.data.u8 = Settings->MotorType;
+    break;
     // open loop settings
     case MotorParams::OPEN_LOOP_MODE_ENABLED:         /// startup in open loop mode 
       packet.rw_address = static_cast<uint8_t>(param);
@@ -107,4 +112,27 @@ void MotorControllerInterface::recieve_packet(MotorControllerPacket_t &packet)
 void MotorControllerInterface::transmit_packet(const MotorControllerPacket_t &packet)
 {
 
+}
+
+void MotorControllerInterface::init_tmc4671(uint8_t motor_type, uint16_t pole_pairs) 
+{
+  // setup the default register values and write them to the tmc4671
+  for(uint8_t i = 0; i < 0x7D; ++i)
+  {
+    tmc4671_writeInt(TMC_DEFAULT_MOTOR, i, tmc4671Registers[i]);
+  }
+
+  // now update the motor type and pole pairs
+  tmc4671_writeInt(TMC_DEFAULT_MOTOR, TMC4671_MOTOR_TYPE_N_POLE_PAIRS, (motor_type << 16) | (pole_pairs) );
+
+  // stuff to setup hall effect sensors here (default config good for now)
+  // turn on polarity flipping, interpolation, and reverse direction
+  tmc4671_writeInt(TMC_DEFAULT_MOTOR, TMC4671_HALL_MODE, 0x00001101);
+  0x2AAA0000, //0x34: 
+  0x80005555, //0x35: 
+  0xD555AAAA, //0x36: 
+  0x04000000, //0x37: 
+  0x00002AAA, //0x38: 
+  // addresses 0x33 - 0x3A
+  
 }
