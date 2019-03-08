00e89bfce8710f688ab532613ab1c68c2c6af907
Sam Ellicott <sellicott@cedarville.edu>
2019-03-08 12:11:16 -0500

    let the USB code go back to the stm intended

--- motor-controller-nucleo/USB_DEVICE/App/usbd_cdc_if.c
+++ motor-controller-nucleo/USB_DEVICE/App/usbd_cdc_if.c
@@ -157,7 +157,7 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 static int8_t CDC_Init_FS(void);
 static int8_t CDC_DeInit_FS(void);
 static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length);
-//static int8_t CDC_Receive_FS(uint8_t* pbuf, uint16_t *Len);
+static int8_t CDC_Receive_FS(uint8_t* pbuf, uint32_t *Len);
 
 /* USER CODE BEGIN PRIVATE_FUNCTIONS_DECLARATION */
 
@@ -288,7 +288,7 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   * @param  Len: Number of data received (in bytes)
   * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
   */
-int8_t CDC_Receive_FS(uint8_t* Buf, uint16_t *Len)
+static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
   uint8_t result = USBD_OK;
