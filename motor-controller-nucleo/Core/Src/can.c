75d55210a1069200df6d2d829af73722649315a4
Samuel  Ellicott <sellicott@cedarville.edu>
2019-03-31 21:37:07 -0400

    Fixed can timing

--- motor-controller-nucleo/Core/Src/can.c
+++ motor-controller-nucleo/Core/Src/can.c
@@ -63,11 +63,11 @@ void MX_CAN_Init(void)
 {
 
   hcan.Instance = CAN;
-  hcan.Init.Prescaler = 5;
+  hcan.Init.Prescaler = 5+1;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_3TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_3TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
   hcan.Init.TimeTriggeredMode = DISABLE;
   hcan.Init.AutoBusOff = DISABLE;
   hcan.Init.AutoWakeUp = DISABLE;
